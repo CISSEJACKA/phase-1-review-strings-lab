45a1982cade433d4599142a0a5635e75255b3cd9
JACKA <jackarcisse@gmail.com>
2024-03-18 22:37:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+const currentUser = 'Jacka';
+
+
+
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+
+
+
+
+
+
+
+
